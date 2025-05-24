85c7ff6ffe4bf77b686686799f7d6e6905b5533f
unknown <brunofrg1212@gmail.com>
2025-05-24 18:30:00 -0300

    commit segundo

---
+++ logica.programaçao.py
@@ -0,0 +1,32 @@
+print("Digite o primeiro número: 7")
+print("Digite o segundo número 9 ")
+print('executa')
+
+idade = 17
+
+if idade >= 18:
+
+print("Voce pode dirigir?")
+
+else:
+    print("Voce n pode dirigir")
+
+print("hello:, world")
+
+if idade = 18
+tem_carteira == "True":
+
+if idade >= and tem_carteira == "True"
+    print("Voce pode dirigir")
+
+else:
+    print("Voce não pode dirigir")
+
+
+idade = 19
+
+if idade >= 19:
+    print("Voce pode dirigir")
+
+else:
+    print("Voce não pode dirigir")
